69137532f67ee6f394b974f5530248477cdb7dac
Ali Sheikh <ali.sheikh@flexport.com>
2021-05-24 22:59:38 -0400

    final

--- hash_preimage.py
+++ hash_preimage.py
@@ -32,18 +32,23 @@ def hash_preimage(target_string):
     length_string_target = len(target_string)
 
     while 1:
+        # random_string = ''.join(random.choice(hash_letters) for i in range(12))
+        # #This will create a random string using the hash_letters
+        # #String will be made up of alpha, numberic, and punctuation
+        # #Create the bytes from the string
+        # random_byte_string = random_string.encode('utf-8')
+        # #Convert random byte string and encode
+        # random_string_sha256 = hashlib.sha256(random_byte_string)
+        # #use sha256 algo
+        # #print(random_string_sha256)
+        # random_string_hex = random_string_sha256.hexdigest()
+        # #print(random_string_hex)
+        # random_string_bin = (bin(int(random_string_hex, 16))[2:]).zfill(256)
+
         random_string = ''.join(random.choice(hash_letters) for i in range(12))
-        #This will create a random string using the hash_letters
-        #String will be made up of alpha, numberic, and punctuation
-        #Create the bytes from the string
-        random_byte_string = random_string.encode('utf-8')
-        #Convert random byte string and encode
-        random_string_sha256 = hashlib.sha256(random_byte_string)
-        #use sha256 algo
-        #print(random_string_sha256)
-        random_string_hex = random_string_sha256.hexdigest()
-        #print(random_string_hex)
-        random_string_bin = (bin(int(random_string_hex, 16))[2:]).zfill(256)
+        random_byte_string = random_string.encode('utf-8').hashlib.sha256().hexdigest()
+        random_string_bin = (bin(int(random_byte_string, 16))[2:]).zfill(256)
+
         #print(random_string_bin)
         #logic for checking code if its is equal
         if (random_string_bin[-length_string_target:] == target_string):
